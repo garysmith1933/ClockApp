b297d40b1c3609b6d085ab7c3e00fdcff972c489
Gary Smith <garysmith193358@gmail.com>
2024-01-16 15:38:19 -0500

    added day to frame

--- src/main/java/org/example/Frame.java
+++ src/main/java/org/example/Frame.java
@@ -8,8 +8,11 @@ import java.util.Calendar;
 public class Frame extends JFrame {
     Calendar calendar;
     SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
     JLabel timeLabel;
+    JLabel dayLabel;
     String time;
+    String day;
 
     Frame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,9 +22,19 @@ public class Frame extends JFrame {
         this.setResizable(false);
 
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE");
+
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Ink Free", Font.PLAIN, 35));
+
         timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 50));
+        timeLabel.setBackground(Color.BLACK);
+        timeLabel.setForeground(new Color(0x00FF00));
+        timeLabel.setOpaque(true);
 
         this.add(timeLabel);
+        this.add(dayLabel);
         this.setVisible(true);
 
         setTime();
@@ -31,6 +44,9 @@ public class Frame extends JFrame {
         while(true) {
             time = timeFormat.format(Calendar.getInstance().getTime());
             timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
 
             try {
                 Thread.sleep(1000);
