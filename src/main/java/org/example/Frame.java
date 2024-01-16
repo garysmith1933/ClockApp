23e1f75c0331cd281e9e2dab78ed09dad276edc2
Gary Smith <garysmith193358@gmail.com>
2024-01-16 15:15:48 -0500

    added current time to frame

--- src/main/java/org/example/Frame.java
+++ src/main/java/org/example/Frame.java
@@ -2,14 +2,28 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Frame extends JFrame {
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    JLabel timeLabel;
+    String time;
+
     Frame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("My Clock Program");
         this.setLayout(new FlowLayout());
         this.setSize(350, 200);
         this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        timeLabel = new JLabel();
+        time = timeFormat.format(Calendar.getInstance().getTime());
+        timeLabel.setText(time);
+
+        this.add(timeLabel);
         this.setVisible(true);
     }
 }
